0395be562939afe57c841596c5269c872d4c066d
RazZzQ <roswell.quispe@tecsup.edu.pe>
2023-10-31 21:43:38 -0500

    .

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,10 +4,11 @@ public class Enemigo : MonoBehaviour
 {
     public float energiaMaxima = 100f;
     public float energiaActual;
-    public float tiempoDescanso = 5f; // Tiempo de descanso en segundos
+    public float tiempoDescanso = 5f;
     private bool descansando = false;
     private float tiempoDescansoActual = 0f;
-    public Nodos objetivoNodo; // El nodo hacia el que se dirige el enemigo
+    public Nodos objetivoNodo;
+
     private Vector2 velocidad;
     public float movimientoTiempo;
 
@@ -25,11 +26,7 @@ public class Enemigo : MonoBehaviour
         {
             if (energiaActual > 0)
             {
-                // Mover al enemigo hacia el objetivoNodo
                 MoverHaciaNodo(objetivoNodo);
-                energiaActual = energiaActual - objetivoNodo.PesoDelArco;
-                Debug.Log(energiaActual);
-                // Si el enemigo llega al objetivoNodo, busca el siguiente nodo
                 if (Vector2.Distance(transform.position, objetivoNodo.transform.position) < 0.1f)
                 {
                     objetivoNodo = GetNextNode();
@@ -37,18 +34,15 @@ public class Enemigo : MonoBehaviour
             }
             else
             {
-                // El enemigo se queda sin energía, inicia el tiempo de descanso
                 descansando = true;
                 tiempoDescansoActual = 0f;
             }
         }
         else
         {
-            // El enemigo está descansando, espera hasta que recupere su energía
             tiempoDescansoActual += Time.deltaTime;
             if (tiempoDescansoActual >= tiempoDescanso)
             {
-                // Descanso completo, el enemigo recupera toda su energía
                 energiaActual = energiaMaxima;
                 descansando = false;
                 objetivoNodo = GetNextNode();
@@ -65,6 +59,7 @@ public class Enemigo : MonoBehaviour
     {
         if (collision.gameObject.tag == "Nodo")
         {
+            energiaActual = energiaActual - objetivoNodo.PesoDelArco;
             objetivoNodo = collision.gameObject.GetComponent<Nodos>().GetNextNode();
         }
     }
